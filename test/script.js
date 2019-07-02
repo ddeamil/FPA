5edf238faba840871105d8defd9b59f3e7e3290b
Desmond de amil <51032990+ddeamil@users.noreply.github.com>
2019-07-02 08:49:00 +0200

    Add files via upload

--- test/script.js
+++ test/script.js
@@ -1,7 +1,19 @@
 
 
-<script>
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+var inkbox = document.getElementById("inked-painted");
+
+var colorbox = document.getElementById("colored");
+
+var fillerImage = document.getElementById("inked");
+
+inkbox.addEventListener("mousemove",trackLocation,false);
+inkbox.addEventListener("touchstart",trackLocation,false);
+inkbox.addEventListener("touchmove",trackLocation,false);
+
+function trackLocation(e){
+	var rect = fillerImage.getBoundingClientRect();
+	var position = ((e.pageX - rect.left) / fillerImage.offsetWidth)*100;
+	if(position <= 100){ 
+       colorbox.style.width = position+"%";
+  }
 }
-</script>
